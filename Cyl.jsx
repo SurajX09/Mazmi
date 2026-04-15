3126a10473b16d182fd737949eeed476e92a7d29
SurajX09 <kolisuraj.dev@gmail.com>
2026-04-15 20:09:27 +0530

    Update Cyl.jsx

--- Cyl.jsx
+++ Cyl.jsx
@@ -16,7 +16,7 @@ const Cyl = () => {
   return (
     <group rotation={[0, 1.4, 0.5]}>
       <mesh ref={cylRef}>
-        <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+        <cylinderGeometry args={[1, 1, 1, 61, 60, true]} />
         <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
       </mesh>
     </group>
